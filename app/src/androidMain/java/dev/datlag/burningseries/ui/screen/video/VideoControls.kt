51e2e47735601d617be47fb678930494a85bc310
DatLag <jeff.retz08@gmail.com>
2022-12-21 02:10:25 +0100

    hide video controls on android

--- app/src/androidMain/java/dev/datlag/burningseries/ui/screen/video/VideoControls.kt
+++ app/src/androidMain/java/dev/datlag/burningseries/ui/screen/video/VideoControls.kt
@@ -1,6 +1,7 @@
 package dev.datlag.burningseries.ui.screen.video
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.IconButton
 import androidx.compose.material.TopAppBar
@@ -18,117 +19,142 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
 import dev.datlag.burningseries.common.SemiBlack
+import dev.datlag.burningseries.common.onClick
 import dev.datlag.burningseries.common.toDuration
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.roundToLong
 
 @Composable
-fun BoxScope.VideoControls(component: VideoComponent) {
-    TopAppBar(
-        backgroundColor = Color.Transparent,
-        contentColor = Color.White,
-        navigationIcon = {
-            IconButton(onClick = {
-                component.onGoBack()
-            }) {
-                Icon(
-                    imageVector = Icons.Default.ArrowBack,
-                    contentDescription = null
-                )
-            }
-        },
-        title = {
-            Text(
-                text = component.episode.title,
-                maxLines = 1
-            )
-        },
-        modifier = Modifier.align(Alignment.TopStart).fillMaxWidth().background(
-            brush = Brush.verticalGradient(
-                colors = listOf(Color.SemiBlack, Color.Transparent)
-            )
-        ),
-        elevation = 0.dp
-    )
+fun VideoControls(component: VideoComponent) {
+    val scope = rememberCoroutineScope()
+    var displayControls by remember { mutableStateOf(true) }
 
-    Row(
-        modifier = Modifier.align(Alignment.Center),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        IconButton(onClick = {
-            component.rewind()
-        }) {
-            Icon(
-                imageVector = Icons.Default.Replay10,
-                contentDescription = null,
-                tint = Color.White
-            )
-        }
-        IconButton(onClick = {
-            component.triggerPlayPause()
-        }) {
-            Icon(
-                imageVector = component.playIcon.subscribeAsState().value,
-                contentDescription = null,
-                tint = Color.White
-            )
-        }
-        IconButton(onClick = {
-            component.forward()
-        }) {
-            Icon(
-                imageVector = Icons.Default.Forward10,
-                contentDescription = null,
-                tint = Color.White
-            )
-        }
+    LaunchedEffect(Unit) {
+        delay(3000)
+        displayControls = false
     }
 
-    Row(
-        modifier = Modifier.align(Alignment.BottomCenter).fillMaxWidth().padding(horizontal = 16.dp).background(
-            brush = Brush.verticalGradient(
-                colors = listOf(Color.Transparent, Color.SemiBlack)
+    Box(modifier = Modifier.fillMaxSize().pointerInput(Unit) {
+        detectTapGestures {
+            displayControls = true
+            scope.launch(Dispatchers.IO) {
+                delay(3000)
+                displayControls = false
+            }
+        }
+    }) {
+        if (displayControls) {
+            TopAppBar(
+                backgroundColor = Color.Transparent,
+                contentColor = Color.White,
+                navigationIcon = {
+                    IconButton(onClick = {
+                        component.onGoBack()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
+                },
+                title = {
+                    Text(
+                        text = component.episode.title,
+                        maxLines = 1
+                    )
+                },
+                modifier = Modifier.align(Alignment.TopStart).fillMaxWidth().background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color.SemiBlack, Color.Transparent)
+                    )
+                ),
+                elevation = 0.dp
             )
-        ),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        val pos by component.position.subscribeAsState()
-        val length by component.length.subscribeAsState()
-        val progress = try {
-            pos * 100 / length
-        } catch (ignored: Throwable) { 0 }.toFloat()
-        var changeableProgress by remember { mutableStateOf(progress) }
 
-        Text(
-            text = pos.toDuration(),
-            textAlign = TextAlign.Center
-        )
-        Slider(
-            modifier = Modifier.weight(1F),
-            value = changeableProgress,
-            onValueChange = {
-                changeableProgress = it
-            },
-            onValueChangeFinished = {
-                component.seekTo(min(max(0, (length * (changeableProgress / 100F)).roundToLong()), length))
-            },
-            valueRange = 0F..100F,
-            colors = SliderDefaults.colors(
-                thumbColor = MaterialTheme.colorScheme.primary,
-                activeTrackColor = MaterialTheme.colorScheme.primary,
-                inactiveTrackColor = Color.White.copy(alpha = 0.2F)
-            )
-        )
-        Text(
-            text = length.toDuration(),
-            textAlign = TextAlign.Center
-        )
+            Row(
+                modifier = Modifier.align(Alignment.Center),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = {
+                    component.rewind()
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Replay10,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+                IconButton(onClick = {
+                    component.triggerPlayPause()
+                }) {
+                    Icon(
+                        imageVector = component.playIcon.subscribeAsState().value,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+                IconButton(onClick = {
+                    component.forward()
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Forward10,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+            }
+
+            Row(
+                modifier = Modifier.align(Alignment.BottomCenter).fillMaxWidth().padding(horizontal = 16.dp).background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color.Transparent, Color.SemiBlack)
+                    )
+                ),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                val pos by component.position.subscribeAsState()
+                val length by component.length.subscribeAsState()
+                val progress = try {
+                    pos * 100 / length
+                } catch (ignored: Throwable) { 0 }.toFloat()
+                var changeableProgress by remember { mutableStateOf(progress) }
+
+                Text(
+                    text = pos.toDuration(),
+                    textAlign = TextAlign.Center
+                )
+                Slider(
+                    modifier = Modifier.weight(1F),
+                    value = changeableProgress,
+                    onValueChange = {
+                        changeableProgress = it
+                    },
+                    onValueChangeFinished = {
+                        component.seekTo(min(max(0, (length * (changeableProgress / 100F)).roundToLong()), length))
+                    },
+                    valueRange = 0F..100F,
+                    colors = SliderDefaults.colors(
+                        thumbColor = MaterialTheme.colorScheme.primary,
+                        activeTrackColor = MaterialTheme.colorScheme.primary,
+                        inactiveTrackColor = Color.White.copy(alpha = 0.2F)
+                    )
+                )
+                Text(
+                    text = length.toDuration(),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
     }
 }
--- app/src/androidMain/java/dev/datlag/burningseries/ui/screen/video/VideoScreen.kt
+++ app/src/androidMain/java/dev/datlag/burningseries/ui/screen/video/VideoScreen.kt
@@ -3,11 +3,10 @@ package dev.datlag.burningseries.ui.screen.video
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import dev.datlag.burningseries.common.onClick
 import kotlinx.coroutines.*
 
 @Composable
