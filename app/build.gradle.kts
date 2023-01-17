943ce7c24796bf9184a3a314702ba1c4b0934f8b
DatLag <jeff.retz08@gmail.com>
2023-01-17 19:48:05 +0100

    increased version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ val exoplayer = "1.0.0-beta03"
 val accompanist = "0.25.1"
 
 val artifact = "dev.datlag.burningseries"
-val appVersion = "4.1.0"
-val appCode = 410
+val appVersion = "4.1.1"
+val appCode = 411
 
 group = artifact
 version = appVersion
