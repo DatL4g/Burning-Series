9b9043e3580352b69900369f6e2d5de7592730aa
DatLag <jeff.retz08@gmail.com>
2023-03-15 21:18:34 +0100

    fixes #34

--- app/src/desktopMain/kotlin/dev/datlag/burningseries/getSystemDarkMode.kt
+++ app/src/desktopMain/kotlin/dev/datlag/burningseries/getSystemDarkMode.kt
@@ -3,14 +3,17 @@ package dev.datlag.burningseries
 import androidx.compose.runtime.Composable
 import dev.datlag.burningseries.other.Constants
 import evalBash
+import org.apache.commons.lang3.SystemUtils
 
 @Composable
 actual fun getSystemDarkMode(): Boolean {
-    val linuxDarkMode = (Constants.LINUX_DARK_MODE_CMD.evalBash(env = null).getOrDefault(String())).ifEmpty {
-        Constants.LINUX_DARK_MODE_LEGACY_CMD.evalBash(env = null).getOrDefault(String())
-    }.contains("dark", true)
-
-    return linuxDarkMode
+    return if (SystemUtils.IS_OS_LINUX) {
+        (Constants.LINUX_DARK_MODE_CMD.evalBash(env = null).getOrDefault(String())).ifEmpty {
+            Constants.LINUX_DARK_MODE_LEGACY_CMD.evalBash(env = null).getOrDefault(String())
+        }.contains("dark", true)
+    } else {
+        false
+    }
 }
 
 @Composable
