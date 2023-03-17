ce064ab981a6c19d421b0926e3bfb20d7be7ebd4
DatLag <jeff.retz08@gmail.com>
2023-03-17 19:47:13 +0100

    fixed crypto security

--- datastore/src/androidMain/java/dev/datlag/burningseries/datastore/CryptoManager.kt
+++ datastore/src/androidMain/java/dev/datlag/burningseries/datastore/CryptoManager.kt
@@ -84,7 +84,7 @@ actual class CryptoManager {
 
     companion object {
         private const val ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
-        private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
+        private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_GCM
         private const val PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
         private const val TRANSFORMATION = "$ALGORITHM/$BLOCK_MODE/$PADDING"
     }
