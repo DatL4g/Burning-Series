f46881a28c0fe82fbca75d37218817d8569a1c1f
DatLag <jeff.retz08@gmail.com>
2023-11-18 19:42:20 +0100

    improve saving and get available streams immediately

--- model/src/commonMain/kotlin/dev/datlag/burningseries/model/state/SaveState.kt
+++ model/src/commonMain/kotlin/dev/datlag/burningseries/model/state/SaveState.kt
@@ -1,10 +1,13 @@
 package dev.datlag.burningseries.model.state
 
 import dev.datlag.burningseries.model.HosterScraping
+import dev.datlag.burningseries.model.Stream
 
 sealed interface SaveState {
     data object Waiting : SaveState
     data class Saving(val data: HosterScraping) : SaveState
+    data class Success(val stream: Stream?) : SaveState
+    data object Error : SaveState
 }
 
 sealed interface SaveAction {
--- network/src/commonMain/kotlin/dev/datlag/burningseries/network/state/SaveStateMachine.kt
+++ network/src/commonMain/kotlin/dev/datlag/burningseries/network/state/SaveStateMachine.kt
@@ -1,22 +1,25 @@
 package dev.datlag.burningseries.network.state
 
 import com.freeletics.flowredux.dsl.FlowReduxStateMachine
+import dev.datlag.burningseries.model.algorithm.MD5
 import dev.datlag.burningseries.model.common.suspendCatching
-import dev.datlag.burningseries.model.state.EpisodeState
 import dev.datlag.burningseries.model.state.SaveAction
 import dev.datlag.burningseries.model.state.SaveState
 import dev.datlag.burningseries.network.Firestore
 import dev.datlag.burningseries.network.JsonBase
 import dev.datlag.burningseries.network.firebase.FireStore
+import dev.datlag.burningseries.network.scraper.Video
 import dev.gitlive.firebase.Firebase
-import dev.gitlive.firebase.auth.FirebaseUser
 import dev.gitlive.firebase.auth.auth
 import dev.gitlive.firebase.firestore.FirebaseFirestore
 import io.ktor.client.*
 import io.realm.kotlin.mongodb.App
 import io.realm.kotlin.mongodb.Credentials
-import io.realm.kotlin.mongodb.User
+import io.realm.kotlin.mongodb.ext.call
+import io.realm.kotlin.types.RealmAny
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class SaveStateMachine(
@@ -50,14 +53,52 @@ class SaveStateMachine(
 
             inState<SaveState.Saving> {
                 onEnter { state ->
-                    val worked = FireStore.addStream(
-                        firebaseUser = StateSaver.firebaseUser,
-                        firestore = firestore,
-                        firestoreApi = firestoreApi,
-                        data = state.snapshot.data.firestore
-                    )
-                    println("Worked: $worked")
-                    state.noChange()
+                    val anySuccess = coroutineScope {
+                        val jsonBaseSaved = async {
+                            suspendCatching {
+                                jsonBase.setBurningSeriesCaptcha(MD5.hexString(state.snapshot.data.href), state.snapshot.data.jsonBase)
+                            }.getOrNull() != null
+                        }
+
+                        val mongoSaved = async {
+                            suspendCatching {
+                                StateSaver.mongoUser!!.functions.call<RealmAny>("add", state.snapshot.data.href, state.snapshot.data.url)
+                            }.getOrNull() != null
+                        }
+
+                        val firebaseSaved = async {
+                            suspendCatching {
+                                FireStore.addStream(
+                                    firebaseUser = StateSaver.firebaseUser,
+                                    firestore = firestore,
+                                    firestoreApi = firestoreApi,
+                                    data = state.snapshot.data.firestore
+                                )
+                            }.getOrNull() ?: false
+                        }
+
+                        return@coroutineScope listOf(jsonBaseSaved.await(), mongoSaved.await(), firebaseSaved.await()).any { it }
+                    }
+
+                    val stream = Video.loadVideos(client, state.snapshot.data.url)
+
+                    if (anySuccess) {
+                        state.override { SaveState.Success(stream) }
+                    } else {
+                        state.override { SaveState.Error }
+                    }
+                }
+            }
+
+            inState<SaveState.Success> {
+                on<SaveAction.Save> { action, state ->
+                    state.override { SaveState.Saving(action.data) }
+                }
+            }
+
+            inState<SaveState.Error> {
+                on<SaveAction.Save> { action, state ->
+                    state.override { SaveState.Saving(action.data) }
                 }
             }
         }
