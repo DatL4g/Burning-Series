8ef239c229f25727e80d56f21c07da235548e6d7
DatLag <jeff.retz08@gmail.com>
2025-05-31 19:07:35 +0200

    bump version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -20,8 +20,8 @@ plugins {
 }
 
 val artifact = "dev.datlag.burningseries"
-val appVersion = "6.1.3"
-val appVersionCode = 613
+val appVersion = "6.1.4"
+val appVersionCode = 614
 
 group = artifact
 version = appVersion
