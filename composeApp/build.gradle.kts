420fd52cdacbbb93f341cdced270b1e26ff40817
Jeff Retz <jeff.retz08@gmail.com>
2025-07-21 17:11:21 +0200

    bump version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -16,8 +16,8 @@ plugins {
 }
 
 val artifact = "dev.datlag.mimasu.extension"
-val appVersion = "10.0.0"
-val appVersionCode = 1000
+val appVersion = "10.1.0"
+val appVersionCode = 1010
 
 group = artifact
 version = appVersion
